7ee1aba08dac5316c7c16711903651444f057df9
Halit Hamza Şeker <seker@halithamza.software>
2022-10-06 12:52:57 +0300

    Firebase handler fixes

--- DataBase/test.py
+++ DataBase/test.py
@@ -1,49 +1,46 @@
 import firebase_admin
+
 from firebase_admin import firestore
 from firebase_admin import credentials
 
-creadpath = r"C:\Users\Engin ORHAN\Desktop\Tübitak\tubitak-2204a-firebase-adminsdk-ddgzc-c44c06eede.json"
-login = credentials.Certificate(creadpath)
-firebase_admin.initialize_app(login)
 
-db = firestore.client()
 
-class Commands():
-    def __init__(self) -> None:
+
+class DatabaseHandler():
+    def __init__(self, credentials_path: str = '') -> None:
+        self.credentials_path = credentials_path
+        self.login = credentials.Certificate(self.credentials_path)
+        firebase_admin.initialize_app(self.login)
+
+        self.db = firestore.client()
+
+
+    def __repr__(self): return str(self.db)
+
+    def __str__(self): return str(self.db)
+
+    def get(self, collection_name: str = '', where_clause: str = '') -> list:
+        customers = self.db.collection(collection_name) 
+
+        if where_clause != '':
+            key, operator, value = (where_clause.split(' '))
+            query: list = customers.where(key, operator, value).get()
+
+            return ( 
+                query
+                if len(query) != 1 
+                else query[0].to_dict()  
+            )   
+
+        return customers.stream() 
+
+    
+    def write(self):
         pass
+        
 
-    def get():
-        customers = db.collection("test").stream()
-
-        for customer in customers:
-            for customer2, var in customer.to_dict().items():
-                print(customer2,":" ,var)
-                print("-"*25)
-
-            #print("{}".format(customer.to_dict()))
-            #person = customer.to_dict("Special Information")
+    
 
 
-    def write(Name, Surname, Age):
-
-        customers = db.collection("test")
-
-        #Adding new Folder to main Collection
-        customers.document(Name).set({
-
-            #Entering the information that Folder will need
-            "Age": Age,
-            "Surname": Surname,
-            "Name": Name,
-        })
-
-#Input for write
-
-Name = input("Name")
-Surname = input("Surname")
-Age = input("Age")
-
-
-r = Commands.write(Name, Surname, Age)
-e = Commands.get() 
-print(e)
+instance = DatabaseHandler('db_credentials.json')
+print(instance.get('test', 'Name == Furkan'))
