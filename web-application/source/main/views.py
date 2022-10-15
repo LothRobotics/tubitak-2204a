80a120bfbf1a7bbc3da9e509ef5d6c10a970742e
FurkanEsen Esen <furkanesen39@gmail.com>
2022-10-15 17:34:16 +0300

    Database handler problem in web app fixed, and some minor changes

--- web-application/source/main/views.py
+++ web-application/source/main/views.py
@@ -14,47 +14,49 @@ NOTIFICATION_TAGS = {
 
 # Create your views here.
 
-def IndexView(request):
-    try:
-         request.session['authentication_account']
+def is_authenticated(request):
+    try: 
+      request.session['authentication_account']
     except KeyError:
-        return redirect('login-page')
+      return False 
     else:
-        return render(request, 'index.html', request.session['authentication_account'])
+      return True 
+
+def IndexView(request):
+    if is_authenticated(request):
+      return render(request, 'index.html', request.session['authentication_account']) 
+    else:
+      return redirect('login-page')
+        
 
 def LoginView(request):
-    if request.POST:
-        try: 
-            request.session['authentication_account']
-        except KeyError:
-            username, password = request.POST['username'], request.POST['password']
+    if request.POST and not is_authenticated(request):
+        username, password = request.POST['username'], request.POST['password']
 
-            if username and password:
-                password_hashed: str = hashlib.md5(password.encode()).hexdigest()
-                user = db_conn.get('accounts', [f'username == {username}', f'password == {password_hashed}'], True)
+        if username and password:
+            password_hashed: str = hashlib.md5(password.encode()).hexdigest()
+            user = db_conn.get('accounts', [f'username == {username}', f'password == {password_hashed}'], True)
 
-                if user:
-                    request.session['authentication_account'] = user[0]
-                    messages.success(request, f'Başarıyla giriş yaptın: {username}', extra_tags=NOTIFICATION_TAGS['success'])
-                    return redirect('index-page')
-                else:
-                    messages.error(request, f'Giriş yapılamadı: {username}', extra_tags=NOTIFICATION_TAGS['error'])
-                    return render(request, 'login.html')
-        else:
-            return IndexView(request)
-
+            if user:
+                request.session['authentication_account'] = user[0]
+                messages.success(request, f'Başarıyla giriş yaptın: {username}', extra_tags=NOTIFICATION_TAGS['success'])
+                return redirect('index-page')
+            else:
+                messages.error(request, f'Giriş yapılamadı: {username}', extra_tags=NOTIFICATION_TAGS['error'])
+                return render(request, 'login.html')
     else:
         return render(request, 'login.html')
 
 def RegisterView(request):
-    return render(request, 'register.html')
-def LogoutView(request):
-    try: 
-        request.session['authentication_account']
-    except KeyError:
-        return redirect('index-page')
+    if request.POST and not is_authenticated(request):
+        pass
     else:
+        return render(request, 'register.html')
+      
+  
+def LogoutView(request):
+    if is_authenticated(request):
         username = request.session['authentication_account']['username']
         messages.info(request, f'Başarıyla çıkış yapıldı: { username }', extra_tags=NOTIFICATION_TAGS['error'])
         del request.session['authentication_account'] 
-        return redirect('login-page')
+    return redirect('login-page')
--- web-application/source/static/src/script.js
+++ web-application/source/static/src/script.js
@@ -1,15 +1,44 @@
-const passwordShowBtn = document.querySelector(".form__password-show-btn"),
-  passwordShowIcon = document.querySelector(".form__password-show-icon"),
-  passwordInput = document.querySelector(".form__password");
+const passwordShowBtns = document.querySelectorAll(".form__password-show-btn"),
+passwordShowIcons = document.querySelectorAll(".form__password-show-icon"),
+passwordInputs = document.querySelectorAll(".form__password");
 
-passwordShowBtn.addEventListener("click", (_e) => {
-  let passwordShowIconName = passwordShowIcon.getAttribute("name");
 
-  if (passwordShowIconName == "eye-off-outline") {
-    passwordShowIcon.setAttribute("name", "eye-outline");
-    passwordInput.setAttribute("type", "text");
-  } else {
-    passwordShowIcon.setAttribute("name", "eye-off-outline");
-    passwordInput.setAttribute("type", "password");
+passwordShowBtns.forEach((passwordShowBtn, index) => {
+  passwordShowBtn. addEventListener("click", (_e) => {
+
+    let passwordShowIcon = passwordShowIcons[index],
+    passwordInput = passwordInputs[index]
+
+    let passwordShowIconName = passwordShowIcon.getAttribute("name");
+  
+    if (passwordShowIconName == "eye-off-outline") {
+      passwordShowIcon.setAttribute("name", "eye-outline");
+      passwordInput.setAttribute("type", "text");
+    } else {
+      passwordShowIcon.setAttribute("name", "eye-off-outline");
+      passwordInput.setAttribute("type", "password");
+    }
+  });
+})
+
+
+
+function formatPhoneNumber(value) {
+  if (!value) return value;
+  const phoneNumber = value.replace(/[^\d]/g, '');
+  const phoneNumberLength = phoneNumber.length;
+  if (phoneNumberLength < 4) return phoneNumber;
+  if (phoneNumberLength < 7) {
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
   }
-});
+  return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+    3,
+    6
+  )}-${phoneNumber.slice(6, 10)}`;
+}
+
+function phoneNumberFormatter() {
+  const inputField = document.querySelector('.form__phonenumber');
+  const formattedInputValue = formatPhoneNumber(inputField.value);
+  inputField.value = formattedInputValue;
+}
