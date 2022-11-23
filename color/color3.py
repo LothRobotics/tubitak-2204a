835f010ca523f8286edd48796a1403ecb66c6895
12Froggo <ferhategeorhan@gmail.com>
2022-11-23 18:06:51 +0300

    Color Palette Generator update

--- color/color3.py
+++ color/color3.py
@@ -4,6 +4,9 @@
 # import matplotlib.patches as patches
 # import matplotlib.image as mpimg
 
+from ast import Pass
+from math import comb
+from turtle import color
 from PIL import Image
 # from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
@@ -12,9 +15,9 @@ import extcolors
 
 from colormap import rgb2hex
 
-input_name = 'SABANCI.jpg'
+img_url = 'color\KABATAS.jpg'
 output_width = 900                   #set the output size
-img = Image.open(input_name)
+img = Image.open(img_url)
 wpercent = (output_width/float(img.size[0]))
 hsize = int((float(img.size[1])*float(wpercent)))
 img = img.resize((output_width,hsize), Image.ANTIALIAS)
@@ -25,18 +28,76 @@ img = img.resize((output_width,hsize), Image.ANTIALIAS)
 
 #read
 
-img_url = input_name
+def removeOccurence(colorList):
+    newList = []
+    for colorIndex in colorList[0]:
+        newList.append(colorIndex[0])
+    return newList
 
 
+def difference(c1, c2):
+    return (abs(c1[0]-c2[0]) + abs(c1[1]-c2[1]) + abs(c1[2]-c2[2])) / 3
+
+def newColor(c1, c2):
+    c1Value = c1[0] + c1[1] + c1[2]
+    c2Value = c2[0] + c2[1] + c2[2]
+
+    if c1Value >= c2Value:
+        return c1
+
+    else:
+        return c2
+
+    # red = round((c1[0] + c2[0]) / 2)
+    # green = round((c1[1] + c2[2]) / 2)
+    # blue = round((c1[2] + c2[2]) / 2)
+    # return (red,green,blue)
+
+def combineColors(colorList, tolerance, colorAmount, tryAmount, tryLimit):
+    changedColors = []
+    tryAmount += 1
+
+    for c1 in colorList:
+        for i in range(1, len(colorList)):
+            #Değerleri Sadeleştirme 
+            c2 = colorList[i]
+            # print(c1, " - ", c2)
+            if c1 == c2:
+                continue
+            
+            result = difference(c1, c2)
+
+            if result < tolerance:
+                combinedColor = newColor(c1, c2)
+
+
+                colorList.remove(c1)
+                colorList.remove(c2)
+                colorList.append(combinedColor)
+                changedColors.append((c1,c2,combinedColor))
+                break
+    print(tryAmount, " - ", tryLimit)
+
+    if tryAmount == tryLimit:
+        return colorList
+
+    if len(colorList) > colorAmount:
+        combineColors(colorList, tolerance, colorAmount, tryAmount, tryLimit)
+
+    return[colorList]
 
 colors_x = extcolors.extract_from_path(img_url, tolerance = 12, limit = 12)
-
 # print(colors_x[0][0][1]) output in RGB
 
-for i in range(len(colors_x[0])):
-    color = colors_x[0][i][0]
-    occurrence = colors_x[0][i][1]
-    print("-"*25)
-    
-    print(color,occurrence)
+# print(colors_x[0])
+# for i in range(len(colors_x[0])):
+#     color = colors_x[0][i][0]
+
+#     print(color)
+
+# print(removeOccurence(colors_x))
+
+tolerance = 30
+colorAmount = 5
+print(combineColors(removeOccurence(colors_x), tolerance, colorAmount,tryLimit=abs((len(colors_x) - colorAmount)), tryAmount=0))
 
